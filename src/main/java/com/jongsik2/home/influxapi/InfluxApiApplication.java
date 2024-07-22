5ef60a8a857342137ce0f22112993d2edf796032
jongsikk <xxx_@kakao.com>
2024-07-22 15:30:27 +0900

    Init: Project Setting

---
+++ src/main/java/com/jongsik2/home/influxapi/InfluxApiApplication.java
@@ -0,0 +1,13 @@
+package com.jongsik2.home.influxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jongsik2/home/influxapi/InfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongsik2.home.influxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
