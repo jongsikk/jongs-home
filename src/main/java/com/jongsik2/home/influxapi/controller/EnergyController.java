28c236b6462d4e95ee0b5e72966ff72cd0c29b3d
jongsikk <xxx_@kakao.com>
2024-07-22 23:39:43 +0900

    Feat: Get Energy By EntityID 추가

---
+++ src/main/java/com/jongsik2/home/influxapi/controller/EnergyController.java
@@ -0,0 +1,23 @@
+package com.jongsik2.home.influxapi.controller;
+
+import com.jongsik2.home.influxapi.dto.Energy;
+import com.jongsik2.home.influxapi.service.EnergyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/energy")
+public class EnergyController {
+    private final EnergyService energyService;
+
+    @GetMapping
+    public List<Energy> energyList(@RequestParam String entityId) {
+        return energyService.energy(entityId);
+    }
+}
