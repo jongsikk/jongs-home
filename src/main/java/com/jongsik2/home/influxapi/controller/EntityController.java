783cbe5d1e12c608b4debbd30907cd06fce87483
jongsikk <xxx_@kakao.com>
2024-08-06 00:23:52 +0900

    Chore: Return Type ResponseEntity<Dto>로 변경

--- src/main/java/com/jongsik2/home/influxapi/controller/EntityController.java
+++ src/main/java/com/jongsik2/home/influxapi/controller/EntityController.java
@@ -1,8 +1,9 @@
 package com.jongsik2.home.influxapi.controller;
 
-import com.jongsik2.home.influxapi.dto.Entity;
+import com.jongsik2.home.influxapi.dto.EntityDto;
 import com.jongsik2.home.influxapi.service.EntityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class EntityController {
     private final EntityService entityService;
 
     @GetMapping
-    public List<Entity> getEntityList() {
-        return entityService.entityList();
+    public ResponseEntity<List<EntityDto>> getEntityList() {
+        return ResponseEntity.ok(entityService.entityList());
     }
 }
