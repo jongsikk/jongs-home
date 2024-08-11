89ec3f9b4133b6ebb50de744234c342ef54a5b06
jongsikk <xxx_@kakao.com>
2024-08-12 00:44:05 +0900

    Chore: Influxdb Dependency 변경 및 Profile 분리

--- src/main/java/com/jongsik2/home/influxapi/config/InfluxDBConfig.java
+++ src/main/java/com/jongsik2/home/influxapi/config/InfluxDBConfig.java
@@ -1,43 +1,24 @@
 package com.jongsik2.home.influxapi.config;
 
-import org.influxdb.dto.Point;
-import org.springframework.beans.factory.annotation.Value;
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.jongsik2.home.influxapi.properties.InfluxDBProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.influxdb.DefaultInfluxDBTemplate;
-import org.springframework.data.influxdb.InfluxDBConnectionFactory;
-import org.springframework.data.influxdb.InfluxDBProperties;
-import org.springframework.data.influxdb.InfluxDBTemplate;
-import org.springframework.data.influxdb.converter.PointConverter;
 
 @Configuration
+@RequiredArgsConstructor
 public class InfluxDBConfig {
-    @Value("${spring.influxdb.url}")
-    String url;
-    @Value("${spring.influxdb.username}")
-    String username;
-    @Value("${spring.influxdb.password}")
-    String password;
-    @Value("${spring.influxdb.database}")
-    String database;
+    private final InfluxDBProperties influxDBProperties;
 
     @Bean
-    public InfluxDBConnectionFactory connectionFactory() {
-        InfluxDBProperties properties = new InfluxDBProperties();
-        properties.setUrl(url);
-        properties.setUsername(username);
-        properties.setPassword(password);
-        properties.setDatabase(database);
-        return new InfluxDBConnectionFactory(properties);
-    }
-
-    @Bean
-    public InfluxDBTemplate<Point> influxDBTemplate(final InfluxDBConnectionFactory connectionFactory) {
-        return new InfluxDBTemplate<>(connectionFactory, new PointConverter());
-    }
-
-    @Bean
-    public DefaultInfluxDBTemplate defaultTemplate(InfluxDBConnectionFactory connectionFactory) {
-        return new DefaultInfluxDBTemplate(connectionFactory);
+    public InfluxDBClient influxDBClient() {
+        return InfluxDBClientFactory.create(
+                influxDBProperties.getUrl(),
+                influxDBProperties.getToken().toCharArray(),
+                influxDBProperties.getOrg(),
+                influxDBProperties.getBucket()
+        );
     }
 }
